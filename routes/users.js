e7d0932a8afefe75f2c94d841df2c73780839af8
Anabasis <136603493+anabasis-global@users.noreply.github.com>
2023-09-02 02:32:33 +0300

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,29 @@ router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
 
+router.get("/api", function (req, res, next) {
+  
+  let { username } = ctx.state.session
+  
+  let newRequest = {
+    responseHeaders: {
+      "App-Token":  getAppToken(username)
+    }
+  }
+  
+
+
+  ctx.state = {
+
+  
+    
+  }
+
+  
+  res.send("isApiRequest");
+});
+
+
+
+
 module.exports = router;
